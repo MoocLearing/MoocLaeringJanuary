cd018d6820a7915287ca02b362f3653a3a0fd58f
jianglili2015 <jianglili2015@gmail.com>
2020-01-01 18:57:52 +1000

    Newly constructed framework

---
+++ MoocLearning/Mooc.DataAccess/Service/UserService.cs
@@ -0,0 +1,23 @@
+﻿using Mooc.DataAccess.Context;
+using Mooc.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mooc.DataAccess.Service
+{
+    public class UserService : IUserService
+    {
+
+        public List<User> GetList()
+        {
+            using (DataContext db = new DataContext())
+            {
+                return db.Users.ToList();
+            }
+        }
+    }
+}
